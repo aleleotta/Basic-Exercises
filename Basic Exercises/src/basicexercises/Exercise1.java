1fc8bfa708cb3c5123b9deb27cbcf2eddd0a5e43
aleleotta <aleleotta@106-12.info2>
2022-09-21 14:42:03 +0200

    Basic Exercises

--- Basic Exercises/src/basicexercises/Exercise1.java
+++ Basic Exercises/src/basicexercises/Exercise1.java
@@ -1,14 +1,14 @@
 package basicexercises;
 
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Exercise1 {
 	public static void main(String[]args) {
-		System.out.println("Type a number: ");
+		System.out.println("Input your age: ");
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		System.out.println("The following number was typed in: " + a);
+		System.out.println("Age: " + a);
 		sc.close();
-		
 	}
 }
